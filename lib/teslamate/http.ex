2e70d72fac0de59d8a5f7dc1c987e61e66ee52cb
senmizu <136210692+senmizu@users.noreply.github.com>
2025-12-15 11:55:19 +0800

    Support NOMINATIM_API_HOST (#30)
    * Maptiler (#28)
    
    * feature: package commit
    
    * add ghcr build trigger, ignore for arm platform
    
    ---------
    
    Signed-off-by: senmizu <136210692+senmizu@users.noreply.github.com>
    
    * Support NOMINATIM_API_HOST
    
    ---------
    
    Signed-off-by: senmizu <136210692+senmizu@users.noreply.github.com>

--- lib/teslamate/http.ex
+++ lib/teslamate/http.ex
@@ -13,7 +13,7 @@ defmodule TeslaMate.HTTP do
       System.get_env("TESLA_API_HOST", "https://owner-api.teslamotors.com") => [
         size: System.get_env("TESLA_API_POOL_SIZE", "10") |> String.to_integer()
       ],
-      "https://nominatim.openstreetmap.org" => [size: 3] ++ nominatim_proxy,
+      System.get_env("NOMINATIM_API_HOST", "https://nominatim.openstreetmap.org") => [size: 3] ++ nominatim_proxy,
       "https://api.github.com" => [size: 1],
       :default => [size: System.get_env("HTTP_POOL_SIZE", "5") |> String.to_integer()]
     }
